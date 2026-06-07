3a4c9b2dc5e8122579eac73026d66601bec1f2ff
kunduyaswani <kunduyaswani@gmail.com>
2026-06-07 21:20:53 +0530

    add checking +ve or -ve

---
+++ check_posnegzero.py
@@ -0,0 +1,8 @@
+n=int(input("enter a number to check positie or negative: "))
+if n>0:
+    print(n ,"is a positive number ")
+elif n<0:
+      print(n ,"is a negitive number ")
+else:
+       print(" n is a zeroo ")
+    
