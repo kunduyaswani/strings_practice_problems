3b24ac022c9c181f5a2e2fca4615c8a0ba8e0cea
kunduyaswani <kunduyaswani@gmail.com>
2026-06-08 10:21:19 +0530

    finding ascii value from the char

--- ascii_to_chr.py
+++ ascii_to_chr.py
@@ -1,2 +1,2 @@
 asciivalue =int(input("enter ASCII value "))
-print("character value of ",ascii,"is",chr(asciivalue))
+print("character value of  ",ascii,"is",chr(asciivalue))
---
+++ find_asciival_from_char.py
@@ -0,0 +1,2 @@
+ch = input("enter a character to find its ascii value ")
+print("ascii value for the given character: ",ord(ch))
