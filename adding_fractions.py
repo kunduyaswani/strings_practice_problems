ed065cf7e276334d4b2bb23e5cd56174c9c92ce0
kunduyaswani <kunduyaswani@gmail.com>
2026-06-10 20:06:07 +0530

    Delete adding_fractions.py

--- adding_fractions.py
+++
@@ -1,9 +0,0 @@
-n1 = (int(input("enter numerator1:  ")))
-d1 =(int(input("eneter  d1 : ")))
-n2 = (int(input("enter numerator2: ")))
-d2 =(int(input("eneter  d2: ")))
-numerator = n1*d2 +n2 *d1
-denominator = d1*d2
-print("After adding the 2 fractions:",numerator,"/",denominator)
-
-
